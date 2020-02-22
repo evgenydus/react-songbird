f1f9261ff3152fd14c2da7db8e9d548250d38d16
evgenydus <evgeny21dus@gmail.com>
2020-02-22 19:43:25 +0300

    WIP: add correct answer selecting

--- src/components/Screen/Options/Option/index.js
+++ src/components/Screen/Options/Option/index.js
@@ -4,12 +4,23 @@ import './index.css'
 const Option = ({ addId, id, isCorrectAnswer, isSelected, name, onClick }) => {
   const handleClick = useCallback(() => {
     onClick(id);
-    addId(id)
-  }, [addId, id, onClick]);
+    if (!isCorrectAnswer) {
+      addId(id)
+    }
+  }, [addId, id, isCorrectAnswer, onClick]);
 
   return (
     <li className="option" onClick={handleClick} >
-      <div className={`option-indicator ${isSelected ? 'indicator-selected' : 'indicator-default'}`} />
+      <div className={`option-indicator ${
+        isSelected ? (
+          isCorrectAnswer ? (
+              'indicator-answer'
+            ) : ('indicator-selected')
+        ) : (
+          'indicator-default'
+        )
+      }`}
+      />
       <span className="option-text" >{name}</span>
     </li>
   )
